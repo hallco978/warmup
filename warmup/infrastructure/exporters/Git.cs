cf99a8e3bd5532e00bea39925e7fdbb31b5408ef
unknown <jamestryand@JT-PC.dayinsure.local>
2014-02-27 02:09:37 +0000

    fixing the git capabilities

--- warmup/infrastructure/exporters/Git.cs
+++ warmup/infrastructure/exporters/Git.cs
@@ -1,56 +1,126 @@
 using System;
 using System.Diagnostics;
-using warmup.infrastructure.extractors;
+// using warmup.infrastructure.extractors;
 using warmup.infrastructure.settings;
+using System.IO;
+using System.Net;
 
 namespace warmup.infrastructure.exporters
 {
     public class Git : BaseExporter
     {
-        public static void Clone(Uri sourceLocation, TargetDir target)
+        public override void Export(string sourceControlWarmupLocation, string templateName, TargetDir targetDir)
         {
-            var separationCharacters = new[] {".git"};
-            string[] piecesOfPath = sourceLocation.ToString().Split(separationCharacters, StringSplitOptions.RemoveEmptyEntries);
-            if (piecesOfPath != null && piecesOfPath.Length > 0)
+            var gitsrc = (new Uri(sourceControlWarmupLocation)).IsFile
+                ? Path.Combine(sourceControlWarmupLocation, templateName)
+                : NewUri(sourceControlWarmupLocation, templateName).AbsoluteUri;
+
+            var destination = targetDir == null
+                ? new TargetDir(Environment.CurrentDirectory)
+                : targetDir;
+
+            var gitSrcPath = TestPath(gitsrc);
+
+            var psi = new ProcessStartInfo("cmd", string.Format(" /c git clone {0} {1}", gitSrcPath, destination.FullPath));
+
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            //todo: better error handling
+            Console.WriteLine("Running: {0} {1}", psi.FileName, psi.Arguments);
+            string output, error = "";
+            using (Process p = Process.Start(psi))
             {
-                string sourceLocationToGit = piecesOfPath[0] + ".git";
+                output = p.StandardOutput.ReadToEnd();
+                error = p.StandardError.ReadToEnd();
+            }
 
-                var psi = new ProcessStartInfo("cmd",string.Format(" /c git clone {0} {1}", sourceLocationToGit, target.FullPath));
+            Console.WriteLine(output);
+            Console.WriteLine(error);
 
-                psi.UseShellExecute = false;
-                psi.CreateNoWindow = true;
-                psi.RedirectStandardOutput = true;
-                psi.RedirectStandardError = true;
+        }
 
-                //todo: better error handling
-                Console.WriteLine("Running: {0} {1}", psi.FileName, psi.Arguments);
-                string output, error = "";
-                using (Process p = Process.Start(psi))
+        private static string TestPath(string path)
+        {
+            return new Uri(path).IsFile
+                ? Directory.Exists(path)
+                    ? path
+                    : AdjustedPath(path)
+                : isValid(path)
+                    ? path
+                    : AdjustedPath(path);
+        }
+
+        private static string AdjustedPath(string path)
+        {
+            return path.EndsWith(".git")
+                ? path
+                : path + ".git";
+        }
+
+        public static bool isValid(string url)
+        {
+            try
+            {
+                var urlReq = (HttpWebRequest)WebRequest.Create(url);
+                var urlRes = (HttpWebResponse)urlReq.GetResponse();
+                var sStream = urlRes.GetResponseStream();
+
+                string read = new StreamReader(sStream).ReadToEnd();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                //Url not valid
+                return false;
+            }
+
+        }
+
+        private static Uri NewUri(string baseUri, string relativeUri)
+        {
+            var r = CreateUri(baseUri, relativeUri);
+            if (r.Item1)
+            {
+                return r.Item2;
+            }
+            else
+            {
+                r = CreateUri(baseUri, "");
+                if (r.Item1)
                 {
-                    output = p.StandardOutput.ReadToEnd();
-                    error = p.StandardError.ReadToEnd();
+                    return r.Item2;
                 }
-
-                Console.WriteLine(output);
-                Console.WriteLine(error);
-
-                var templateName = piecesOfPath[1];
-                GitTemplateExtractor extractor = new GitTemplateExtractor(target, templateName);
-                extractor.Extract();
-                //string git_directory = Path.Combine(target.FullPath, ".git");
-                //if (Directory.Exists(git_directory))
-                //{
-                //    Console.WriteLine("Deleting {0} directory", git_directory);
-                //    Directory.Delete(git_directory, true);
-                //}
+                else
+                {
+                    throw new ArgumentException("The base is not valid");
+                }
             }
         }
 
-        public override void Export(string sourceControlWarmupLocation, string templateName, TargetDir targetDir)
+        private static Tuple<bool, Uri> CreateUri(string baseUri, string relativeUri)
         {
-            var baseUri = new Uri(WarmupConfiguration.settings.SourceControlWarmupLocation + templateName);
-            Console.WriteLine("git exporting to: {0}", targetDir.FullPath);
-            Clone(baseUri, targetDir);
+            return CreateUri(
+                    baseUri.EndsWith("/")
+                    ? new Uri(baseUri)
+                    : new Uri(baseUri + "/"),
+                    relativeUri);
+        }
+
+        private static Tuple<bool,Uri> CreateUri(Uri baseUri, string relativeUri) {
+            Uri ret;
+            return Tuple.Create(Uri.TryCreate(baseUri, relativeUri, out ret), ret); 
+        } 
+
+        public static void Clone(Uri sourceLocation, TargetDir target)
+        {
+        }
+
+        public static void Clone(string sourceLocation, TargetDir target)
+        {
         }
     }
 }
